9f752bad8768cd14048e9751d440eca27a43975b
keertisingh8810-cmd <keertisingh8810@gmail.com>
2025-12-18 11:39:38 +0530

    Day 2- Variables

---
+++ day2_variables.py
@@ -0,0 +1,14 @@
+name= "Keerti"
+age= 21
+height= 5.6
+is_working= True
+
+print(name)
+print(age)
+print(height)
+print(is_working)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_working))
