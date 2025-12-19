0a8822fac88bbb5fb104b7158ebd9e6c075366cd
keertisingh8810-cmd <keertisingh8810@gmail.com>
2025-12-19 15:54:38 +0530

    Day-3_input.py

---
+++ day3_input and operators.py
@@ -0,0 +1,72 @@
+#input and operators
+a= input("Enter your name: ")
+print("My name is",a )
+
+x= input("Enter first number: ")
+y= input("Enter second number: ")
+print(x+y)
+
+print(int(x)+int(y))
+
+#operators
+#Arithmetic operator
+#Assignment operator
+#Comparison operator
+#Logical operator
+#Identity operator
+#Membership operator
+#Bitwise operator
+
+#Arithmetic opea=rator
+print("5+6 is ", 5+6)
+print("5-6 is ", 5-6)
+print("5*6 is ", 5*6)
+print("5/6 is ", 5/6)
+print("5**3 is ", 5**3)
+print("5%5 is ", 5%5)
+print("15//6 is ", 15//6)
+
+#Assignment operator
+x=5
+print(x)
+x+=7
+x-=7
+x/=7
+x%=7 #x= x%7
+print(x)
+
+#Comparison operator
+i=5
+print(i==5)
+print(i!=5)
+print(i>=5)
+print(i>5)
+
+#Logical operator
+a= True
+b= False
+print(a and a)
+print(a and b)
+print(b and b)
+print(a or b)
+
+#Identity operator
+a=5
+b=9
+print(a is b)
+print(a is not b)
+print(a is a)
+
+#Membership operator
+list= [3,3,2,2, 39,33,35,32]
+print(324 in list)
+print(324 not in list)
+
+#Bitwise operator
+#0-00, 1-01, 2-10, 3-11
+
+print(0 & 1)
+print(0 & 2)
+print(0 | 1)
+print(0 | 3)
+
